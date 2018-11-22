9692a7341e00d33a657e23e9bd3570e0a081c094
Johan Borestad <johan.borestad@netent.com>
2018-11-22 14:50:04 +0100

    100% test coverage

--- src/delay.ts
+++ src/delay.ts
@@ -9,17 +9,13 @@ export enum Delays {
 
 /**
  * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
  */
-async function delayedHello(name: string, delay: number = Delays.Medium) {
+async function delayedPromise(name: string, delay: number) {
   return new Promise<string>(resolve =>
     setTimeout(() => resolve(`Hello, ${name}`), delay)
   )
 }
 
 export async function greeter(name: string) {
-  return delayedHello(name, Delays.Long)
+  return delayedPromise(name, Delays.Long)
 }
