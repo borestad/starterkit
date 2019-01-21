fd75e0094b2b19dc1bc933b9ca5eabb7a57abb9f
Johan Borestad <johan.borestad@netent.com>
2019-01-21 10:50:43 +0100

    Add example-lib2

---
+++ examples/example-lib2/src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { NAME } from '../'
+
+test('Something', () => {
+  expect(NAME).toBe('example-lib2')
+})
---
+++ examples/example-lib2/src/index.ts
@@ -0,0 +1,7 @@
+import { add } from '@scope/example-lib1'
+
+export const NAME = 'example-lib2'
+
+export const someFn = () => {
+  return add(1, 2)
+}
