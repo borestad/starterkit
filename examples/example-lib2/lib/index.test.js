fdbfa4f53f50260c12e3adf9bbf6a8a929157c33
Johan Borestad <johan.borestad@netent.com>
2019-03-29 15:11:34 +0100

    Remove unused files

--- examples/example-lib2/lib/index.test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var _ = require('./')
-
-test('Something', function() {
-  expect((0, _.double)(10)).toBe(20)
-})
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50ZXN0LnRzIl0sIm5hbWVzIjpbInRlc3QiLCJleHBlY3QiLCJ0b0JlIl0sIm1hcHBpbmdzIjoiOztBQUFBOztBQUVBQSxJQUFJLENBQUMsV0FBRCxFQUFjLFlBQU07QUFDdEJDLEVBQUFBLE1BQU0sQ0FBQyxjQUFPLEVBQVAsQ0FBRCxDQUFOLENBQW1CQyxJQUFuQixDQUF3QixFQUF4QjtBQUNELENBRkcsQ0FBSiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGRvdWJsZSB9IGZyb20gJy4vJ1xuXG50ZXN0KCdTb21ldGhpbmcnLCAoKSA9PiB7XG4gIGV4cGVjdChkb3VibGUoMTApKS50b0JlKDIwKVxufSlcbiJdfQ==
