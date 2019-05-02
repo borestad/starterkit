5f514f893ea16bfa5b6770f5732bd4efeac7d05a
Johan Borestad <johan.borestad@netent.com>
2019-05-03 00:05:11 +0200

    test

--- bin/validate.ts
+++ bin/validate.ts
@@ -12,6 +12,10 @@ let result: any
  * Validate if files are ignored
  */
 
+
+
+ 
+
 run(async function sanityCheck () {
   if (isEmpty((result = GIT.IGNORED_FILES_STILL_IN_INDEX))) {
     log(`${tick} ️Validating ignored files`)
