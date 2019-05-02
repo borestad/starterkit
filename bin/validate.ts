8cdeaeb0bbc07b12c6d7e43d2d5348c5bbe00e0f
Johan Borestad <johan.borestad@netent.com>
2019-05-02 23:43:21 +0200

    Improve CI error messages

--- bin/validate.ts
+++ bin/validate.ts
@@ -1,26 +1,33 @@
 #!/usr/bin/env npx ts-node -T
 
-import { GIT } from '@config/cli-tools'
+import { GIT, run } from '@config/cli-tools'
+import { tick } from 'figures'
 import { isEmpty } from 'lodash'
 import * as util from 'util'
+const { log } = console
 
-let files
+let result: any
 
 /**
  * Validate if files are ignored
  */
-if (!isEmpty((files = GIT.IGNORED_FILES_STILL_IN_INDEX))) {
-  throw new Error(
-    util.format(
-      `
-      There exists committed files in this repository that are ignored by git.
-      This usually happens when files are beeing added to .gitignore before deleting them.
+
+run(async function sanityCheck () {
+  if (isEmpty((result = GIT.IGNORED_FILES_STILL_IN_INDEX))) {
+    log(`${tick} ️Validating ignored files`)
+  } else {
+    throw new Error(
+      util.format(
+        `
+        There exists committed files in this repository that are ignored by git.
+        This usually happens when files are beeing added to .gitignore before deleting them.
 
 
-      This can cause a lot of unwanted side effects - Please remove them!
-      https://www.google.com/search?q=delete+files+still+in+in+gitignore
-      \n%s`,
-      JSON.stringify(files, null, 4)
+        This can cause a lot of unwanted side effects - Please remove them!
+        https://www.google.com/search?q=delete+files+still+in+in+gitignore
+        \n%s`,
+        JSON.stringify(result, null, 4)
+      )
     )
-  )
-}
+  }
+})
