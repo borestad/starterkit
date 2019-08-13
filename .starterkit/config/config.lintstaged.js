a4926c2a50140821c446314e7906ddf2ae36c9e9
Johan Borestad <johan.borestad@netent.com>
2019-08-13 17:19:30 +0200

    Add format-package to lint-staged

--- .starterkit/config/config.lintstaged.js
+++ .starterkit/config/config.lintstaged.js
@@ -10,6 +10,7 @@ console.log(`${yellow(`⭐ ${underline('lint-staged')}`)}`)
 
 module.exports = {
   '.gitignore': ['node node_modules/@netent/starterkit/bin/validate'],
-  '*.{json,md,html,css}': ['prettier -c --write', 'git add'],
-  '*.{js,jsx,ts,tsx}': ['npx eslint --fix --ext .js,.jsx,.ts,.tsx --cache', 'git add']
+  '*.{json,md,html,css}': ['npx prettier -c --write', 'git add'],
+  '*.{js,jsx,ts,tsx}': ['npx eslint --fix --ext .js,.jsx,.ts,.tsx --cache', 'git add'],
+  'package.json': ['npx format-package -w', 'git add']
 }
