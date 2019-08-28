5a236ff5323861e8f1f7e6af2c49e06b2958c27d
Johan Borestad <johan.borestad@netent.com>
2019-08-28 11:21:20 +0200

    Fix broken build

--- examples/example-lib2/jest.config.js
+++ examples/example-lib2/jest.config.js
@@ -4,6 +4,7 @@
  *
  */
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('ts-node/register/transpile-only')
 
 module.exports = require('@netent/starterkit/config/config.jest.base')({
